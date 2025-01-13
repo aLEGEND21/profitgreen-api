d9d8f0cef3c969529c03644dd1a4b6979fc1a43d
aLEGEND21 <arnavmurthi@gmail.com>
2025-01-13 17:02:17 -0500

    chore: Add example user ID for portfolio API

--- src/api/portfolio.py
+++ src/api/portfolio.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Path
 
 from database import get_portfolio
 
@@ -18,33 +18,51 @@ async def view_portfolio_overview(portfolio=Depends(get_portfolio)):
 
 
 @router.get("/history/{user_id}")
-async def get_history(user_id: int, portfolio=Depends(get_portfolio)):
+async def get_history(
+    user_id: str = Path(
+        ..., example="416730155332009984"
+    ),  # Typehint as str and not int to prevent the value being malformed during the request
+    portfolio=Depends(get_portfolio),
+):
     """
     Retrieves the trade history for a user, including the timestamp, ticker, and quantity of shares,
     and price each trade was executed at.
     """
+    if not user_id.isdigit():
+        return HTTPException(status_code=400, detail="Invalid user ID")
+    user_id = int(user_id)
+
     user = await portfolio.find_one({"_id": user_id})
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
     return {
-        "user_id": user["_id"],
+        "user_id": str(user["_id"]),
         "trade_history": user["trade_history"],
     }
 
 
 @router.get("/holdings/{user_id}")
-async def get_holdings(user_id: int, portfolio=Depends(get_portfolio)):
+async def get_holdings(
+    user_id: int = Path(
+        ..., example="416730155332009984"
+    ),  # Typehint as str and not int to prevent the value being malformed during the request
+    portfolio=Depends(get_portfolio),
+):
     """
     Retrieves the current holdings for a user, including the ticker and quantity of shares.
     """
+    if not user_id.isdigit():
+        return HTTPException(status_code=400, detail="Invalid user ID")
+    user_id = int(user_id)
+
     user = await portfolio.find_one({"_id": user_id})
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
 
     return {
-        "user_id": user["_id"],
+        "user_id": str(user["_id"]),
         "holdings": user["portfolio"],
     }
