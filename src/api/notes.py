160b7450f6ae631dfc47ca79a91fd63e035f79bd
aLEGEND21 <arnavmurthi@gmail.com>
2025-01-13 08:06:50 -0500

    feat: Add endpoint for creating notes

--- src/api/notes.py
+++ src/api/notes.py
@@ -1,9 +1,20 @@
+from datetime import datetime
 from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
 
 from database import get_notes
 
 
 router = APIRouter()
+
+
+class NoteCreate(BaseModel):
+    """
+    Represents a new note that will be created.
+    """
+
+    author: str
+    content: str
 
 
 @router.get("/")
@@ -16,3 +27,14 @@ async def get_notes_overview(notes=Depends(get_notes)):
         "count": await notes.count_documents({}),
         "num_authors": len(await notes.distinct("author")),
     }
+
+
+@router.post("/new")
+async def create_note(note: NoteCreate, notes=Depends(get_notes)):
+    """
+    Create a new note for other API users to view and update.
+    """
+    new_note = note.dict()
+    new_note.update({"created": str(datetime.now()), "updated": str(datetime.now())})
+    res = await notes.insert_one(new_note)
+    return {"detail": "Note created successfully", "id": str(res.inserted_id)}
